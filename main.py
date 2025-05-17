4ffe3149ca857f712a6c70fbd19af8b5450ca7eb
Bhavana029 <mekalabhavana029@gmail>
2025-05-17 10:15:36 +0530

    third commit

--- main.py
+++ main.py
@@ -75,4 +75,5 @@ def match_resumes():
 if __name__ == '__main__':
     os.makedirs("uploads", exist_ok=True)
 
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
